2c0a2d00ffe8ff5caa2bebcb6763c924bb16723c
KevinJose37 <kvargas9@udi.edu.co>
2024-02-28 12:37:06 -0500

    Cambios garzón

---
+++ database/migrations/2024_02_28_045341_add_rol_column.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string("rol_name");
+        });
+
+        
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger("rol_id");
+            $table->foreign('rol_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
