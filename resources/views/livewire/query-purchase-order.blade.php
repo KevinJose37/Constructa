da0a3e9c1a733e4dc722cc1b7a5c26be672855e3
KevinJose37 <kvargas9@udi.edu.co>
2024-08-04 19:15:14 -0500

    fix

--- resources/views/livewire/query-purchase-order.blade.php
+++ resources/views/livewire/query-purchase-order.blade.php
@@ -2,6 +2,7 @@
     <x-page-title title="Tabla de órdenes de compra"></x-page-title>
     <x-table>
 
+            
         <div class="row w-100">
             <div class="col-lg-6 w-25">
                 <!-- Div a la izquierda -->
@@ -16,7 +17,7 @@
             <table class="table table-striped table-centered mb-0">
                 <thead>
                     <tr class="d-flex">
-                        <th class="col-md-2">Nombre del proyecto</th>
+                        <th class="col-md-2">Nombre de la orden</th>
                         <th class="col-md-2">Nombre contratista</th>
                         <th class="col-md-2">Nombre compañía destino</th>
                         <th class="col-md-2">Total</th>
@@ -29,7 +30,7 @@
                 <tbody>
                     @foreach ($purchaseOrder as $order)
                     <tr id="purchaseOrderRow_{{ $order->id }}" class="d-flex" data-bs-toggle="collapse" data-bs-target="#collapse{{ $order->id }}" aria-expanded="false" aria-controls="collapse{{ $order->id }}">
-                        <td class="col-md-2">{{ $order->project->project_name }}</td>
+                        <td class="col-md-2">{{ $order->order_name }}</td>
                         <td class="col-md-2">{{ $order->contractor_name }}</td>
                         <td class="col-md-2">{{ $order->company_name }}</td>
                         <td class="col-md-2">{{ $order->total_payable }} COP</td>
@@ -38,11 +39,14 @@
                         <td class="col-md-1"><livewire:purchase-order-paid-information :order="$order" :wire:key="'purchase-order-paid-' . $order->id"></livewire:purchase-order-paid-information>
                         </td>
 
-                        <td style="display: flex; align-items: center;" class="col-md-1">
-                            <a href="#" class="text-reset fs-19 px-1 delete-project-btn" wire:click.prevent="destroyAlert({{ $order->id }}, '{{ $order->project->project_name }}')">
-                                <i class="ri-delete-bin-2-line"></i></a>
-                            <a href="{{ route('purchaseorder.get', ['id' => $order->id]) }}"><i class="ri-search-eye-line"></i></a>
-                        </td>
+                        <td class="col-md-1" style="display: flex; align-items: center;">
+            <a href="#" class="text-reset fs-19 px-1 delete-project-btn" wire:click.prevent="destroyAlert({{ $order->id }})" style="margin-right: 10px;">
+                <i class="ri-delete-bin-2-line"></i></a>
+            <a href="{{ route('purchaseorder.get', ['id' => $order->id]) }}" style="margin-right: 10px;">
+                <i class="ri-search-eye-line"></i></a>
+            <a href="#" style="margin-right: 10px;">
+                <i class="ri-file-upload-fill"></i></a>
+        </td>
                     </tr>
                     <!-- Fila de detalles -->
                     <tr id="collapse{{ $order->id }}" class="collapse">
