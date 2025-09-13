aa03713fe39ae3ec903e8d12dc1f31c438756e48
KevinJose37 <kvargas9@udi.edu.co>
2025-09-13 11:52:42 -0500

    chore: archivo de configuracion

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/views/assets/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
