c0a94fd6280ad583336b4752b97126fcce8fd578
KevinJose37 <kvargas9@udi.edu.co>
2025-09-14 10:42:33 -0500

    chore: sube archivo de configuracion para ejecutar migraciones en despliegue

---
+++ deploy.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Support\Facades\Artisan;
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+Artisan::call('migrate', ["--force" => true]);
+
+echo "Migraciones ejecutadas correctamente!";
